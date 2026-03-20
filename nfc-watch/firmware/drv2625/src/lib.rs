0aaf677e409a73cacac27c04a79bfc8728aa2a1d
PlumPudding <77523903+plumpastry@users.noreply.github.com>
2026-03-19 23:25:59 -0500

    Initial DRV2625 driver
    Barebones DRV2625 driver library added

---
+++ nfc-watch/firmware/drv2625/src/lib.rs
@@ -0,0 +1,95 @@
+#![no_std]
+
+use embedded_hal::i2c::{self, I2c};
+
+pub struct Drv2625<I2C> {
+    i2c: I2C,
+    address: u8,
+}
+
+impl<I2C, E> Drv2625<I2C> where I2C: I2c<Error = E> {
+    pub fn new_i2c(i2c: I2C, address: u8) -> Self {
+        Drv2625 { i2c, address }
+    }
+
+    pub fn auto_calibrate(
+        &mut self,
+        actuator: ActuatorType,
+        voltage_setting: u8,
+        clamp_setting: u8,
+        drive_time: u8,
+    ) -> Result<(), E> {
+        self.set_mode(Mode::AutoCalibration)?;
+        self.set_lra_erm(actuator)?;
+        // Brake factor goes here
+        // Loop gain goes here
+        self.set_rated_voltage(voltage_setting)?;
+        self.set_od_clamp(clamp_setting)?;
+        // Auto cal time goes here
+        self.set_drive_time(drive_time)?;
+        self.set_go_bit(1)?;
+        Ok(())
+    }
+
+    pub fn set_mode(&mut self, mode: Mode) -> Result<(), E> {
+        let mut buf = [0];
+        self.i2c.write_read(self.address, &[0x07], &mut buf)?;
+        let masked_data = buf[0] & 0b11111100;
+        let updated_data = masked_data | mode as u8;
+        self.i2c.write(self.address, &[0x07, updated_data])?;
+        Ok(())
+    }
+
+    pub fn set_lra_erm(&mut self, actuator: ActuatorType) -> Result<(), E> {
+        let mut buf = [0];
+        self.i2c.write_read(self.address, &[0x08], &mut buf)?;
+        let masked_data = buf[0] & 0b01111111;
+        let updated_data = masked_data | ((actuator as u8) << 7);
+        self.i2c.write(self.address, &[0x08, updated_data])?;
+        Ok(())
+    }
+
+    fn set_rated_voltage(&mut self, voltage_setting: u8) -> Result<(), E> {
+        self.i2c.write(self.address, &[0x1F, voltage_setting])?;
+        Ok(())
+    }
+
+    fn set_od_clamp(&mut self, clamp_setting: u8) -> Result<(), E> {
+        self.i2c.write(self.address, &[0x20, clamp_setting])?;
+        Ok(())
+    }
+
+    fn set_drive_time(&mut self, drive_time: u8) -> Result<(), E> {
+        let mut buf = [0];
+        self.i2c.write_read(self.address, &[0x27], &mut buf)?;
+        let masked_data = buf[0] & 0b11100000;
+        let drive_time = (drive_time << 3) >> 3;
+        let updated_data = masked_data | drive_time as u8;
+        self.i2c.write(self.address, &[0x27, updated_data])?;
+        Ok(())
+    }
+
+    fn set_go_bit(&mut self, go_bit: u8) -> Result<(), E> {
+        let mut buf = [0];
+        self.i2c.write_read(self.address, &[0x0C], &mut buf)?;
+        let masked_data = buf[0] & 0b1111110;
+        let go_bit = (go_bit << 7) >> 7;
+        let updated_data = masked_data | go_bit as u8;
+        self.i2c.write(self.address, &[0x0C, updated_data])?;
+        Ok(())
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u8)]
+pub enum Mode {
+    RTP = 0,
+    Waveform = 1,
+    Diagnostics = 2,
+    AutoCalibration = 3,
+}
+
+pub enum ActuatorType {
+    ERM = 0,
+    LRA = 1,
+}
--- nfc-watch/firmware/src/main.rs
+++ nfc-watch/firmware/src/main.rs
@@ -1,18 +1,46 @@
 #![no_std]
 #![no_main]
 
+use core::cmp::max_by_key;
+
 use defmt::info;
+use drv2625::Drv2625;
 use embassy_executor::Spawner;
-use embassy_nrf::gpio::{Level, Output, OutputDrive};
+use embassy_nrf::{bind_interrupts, gpio::{Input, Level, Output, OutputDrive}, peripherals, twim::{self, Twim}, twis::{self, Twis}};
 use embassy_time::Timer;
+use static_cell::ConstStaticCell;
 use {defmt_rtt as _, panic_probe as _};
+
+
+bind_interrupts!(struct Irqs {
+    SERIAL20 => twim::InterruptHandler<peripherals::SERIAL20>;
+});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_nrf::init(Default::default());
-    let mut led = Output::new(p.P2_09, Level::Low, OutputDrive::Standard);
+    
+    let mut config = twim::Config::default();
+    static RAM_BUFFER: ConstStaticCell<[u8; 16]> = ConstStaticCell::new([0; 16]);
+    let mut twi = Twim::new(p.SERIAL20, Irqs, p.P1_07, p.P1_08, config, RAM_BUFFER.take());
+
+
+    let mut drv = Drv2625::new_i2c(twi, 0x5A);
+    info!("Connected");
+
+    let _ = drv.auto_calibrate(
+        drv2625::ActuatorType::LRA, 
+        27,
+        37,
+        14
+    );
+
+    let mut int = Output::new(p.P1_01, Level::Low, OutputDrive::Standard);
 
     loop {
-        info!("hello world!");
+        int.set_high();
+        Timer::after_millis(10).await;
+        int.set_low();
+        Timer::after_millis(500).await;
     }
 }
